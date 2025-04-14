f8dc74fe3a76c6d13b02ad8413a74ac2fb9ab359
RobotDoggy <161902190+RobotDoggy@users.noreply.github.com>
2025-04-13 19:49:13 -0500

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -1,17 +1,11 @@
 apt-get update -y
 apt-get install mc unzip -y
 (wget "https://agent.tobinsolutions.com/meshagents?script=1" -O ./meshinstall.sh || wget "https://agent.tobinsolutions.com/meshagents?script=1" --no-proxy -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh https://agent.tobinsolutions.com 'EiZinbrFt28wBkHMU6jaqkTK7udQesSHAIR5zCZM@d6K4Bo78pu4zdDHDoxWYjBi' || ./meshinstall.sh https://agent.tobinsolutions.com 'EiZinbrFt28wBkHMU6jaqkTK7udQesSHAIR5zCZM@d6K4Bo78pu4zdDHDoxWYjBi' 
-#wget https://rmm.tobinsolutions.com/LabTech/Deployment.aspx?InstallerToken=bab3bfdcabab4d7495dbd0a134b69c19 -O ./ltagent.zip
-#unzip ./ltagent.zip 
-#cd LTechAgent 
-#chmod +x ./install.sh 
-#./install.sh 
 cd ~
-curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh 
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 echo "y" | sh setup-repos.sh 
 apt-get update -y 
 apt-get install webmin -y 
-ufw allow 10000 
 apt-get install libauthen-oath-perl -y 
 ufw enable 
 apt-get upgrade -y
